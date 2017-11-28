116bf980741ae8b4e14b29d0165df3683162166e
JD Wheelehan <jdwheelehan@gmail.com>
2017-11-27 18:57:34 -0600

    finished and demoing

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,8 +4,11 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -22,6 +25,26 @@ public class SearchController {
         return "search";
     }
 
+
+
+
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String searchresults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList jobs;
+
+        if (searchType.equals("all")){
+            jobs = JobData.findByValue(searchTerm);
+
+        }else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "search";
+    }
+    //@RequestParam searchType, @RequestParam searchTerm)
+
 
 }
